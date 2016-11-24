147143e3202a8982b74a97c34b38f86947569f16
_yecs work <_yecs work>
2016-11-24 18:07:32 +0800

    修改测试

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,6 +11,7 @@ class AppComponent extends React.Component {
       <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <h1>测试</h1>
       </div>
     );
   }
